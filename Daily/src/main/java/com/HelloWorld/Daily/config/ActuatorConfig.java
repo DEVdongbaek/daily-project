fb2c8cdc26ad550cada73b6944e89904fd192c33
DEVdongbaek <yardyard@likelion.org>
2025-01-11 17:34:01 +0900

    Changes

---
+++ Daily/src/main/java/com/HelloWorld/Daily/config/ActuatorConfig.java
@@ -0,0 +1,15 @@
+package com.HelloWorld.Daily.config;
+
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorConfig {
+
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
+
+}
